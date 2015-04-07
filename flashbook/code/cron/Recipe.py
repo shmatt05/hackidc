11e38e95edbd67df598ad83d68a5e7e73185577b
Ari Propper <Ari@MacBook-Pro.local>
2015-04-07 17:18:28 +0300

    no message

--- flashbook/code/cron/Recipe.py
+++ flashbook/code/cron/Recipe.py
@@ -1,1 +1,13 @@
 __author__ = 'Ari'
+
+class Recipe(object):
+
+    def parsed_recipe(recipe):
+        return dict(
+            id=recipe.id,
+
+            #TODO: Add all fields to be transmitted to tomer API.
+
+        )
+
+
--- flashbook/code/cron/handlers.py
+++ flashbook/code/cron/handlers.py
@@ -1,1 +1,21 @@
 __author__ = 'Ari'
+from shared.framework import Handler
+import webapp2
+import Recipe
+
+class checkRecipeHandler(webapp2.RequestHandler):
+
+    def post(self):
+        recipe_id = self.request.get('recipe_id')
+        recipe = self.data_service.get_entity_by_key(recipe_id)
+        params_dict = Recipe.parsed_recipe(recipe)
+
+        #TODO: get_results_from_tomer
+    #TODO: def get_Results_from_tomer
+
+
+
+
+
+
+
