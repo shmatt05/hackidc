2cd241e71af40841c6e4b4d03e7a6846c257e9a0
temp-defualt <temp.defualt@gmail.com>
2015-04-09 05:39:07 +0300

    tomer - test booking

--- flashbook/code/website/js/add_recipe.js
+++ flashbook/code/website/js/add_recipe.js
@@ -31,24 +31,51 @@ $(function () {
 
     });
   });
-  
+  booking_condition
   function Book() {
-  alert("asd");
+  var booking_condition;
+  booking_condition = {
+        type: "flight",
+        origin: "TLV",
+        destination:"LAS",
+        booking_start_date:"2015-04-07T23:52:05.665530Z",
+        booking_end_date:"2015-04-27T23:52:05.665530Z",
+        max_price"1,
+        min_duration:7,
+        max_duration:8,
+        number_of_connections:2,
+        max_flight_duration:40,
+        number_of_adult_tickets:1
+    }
+  $.ajax({
+        url : 'http://flashbook-app.appspot.com/api/recipe/all',
+        dataType : 'json',
+        //contentType : 'application/json; charset=UTF-8',
+        type : 'POST',
+        data : {
+            title : "",
+            booking_condition : booking_condition
+        }
+    }).done(function(data, textStatus, jqXHR) {
+        console.log(data);
+    }).fail(function() {
+       console.log(msg);
+    });
+    
+    
   }
-  
-  function login_data() {
-                $.ajax({
-                    url : 'http://flashbook-app.appspot.com/api/user/login',
-                    dataType : 'json',
-                    //contentType : 'application/json; charset=UTF-8',
-                    type : 'POST'
-                }).done(function(data, textStatus, jqXHR) {
-                  console.log(data);
-                  
-                  $(".user").append("Hello " + data.response.authenticated_user.name); 
-
-                }).fail(function(msg) {
-                    console.log(msg);
-                    return false;
-                });
-            }
+ /*
+  "title":"first",
+"booking_condition": {
+"type":"flight",
+"origin":"TLV",
+"destination":"LAS",
+"booking_start_date":"2015-04-07T23:52:05.665530Z",
+"booking_end_date":"2015-04-27T23:52:05.665530Z",
+"max_price":1,
+"min_duration":7,
+"max_duration":8,
+"number_of_connections":2,
+"max_flight_duration":40,
+"number_of_adult_tickets":1
+*/
